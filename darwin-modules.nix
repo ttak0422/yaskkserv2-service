a7ddaf09a98da4df4742e438e87bd133d7cae2b7
ttak0422 <ttak0422@gmail.com>
2024-04-11 23:13:37 +0900

    initial commit

---
+++ darwin-modules.nix
@@ -0,0 +1,110 @@
+self:
+{ config, lib, pkgs, ... }:
+let
+  inherit (lib) types mkOption mkEnableOption concatStringsSep;
+  inherit (self.packages.${pkgs.stdenv.hostPlatform.system}) yaskkserv2;
+  cfg = config.services.yaskkserv2;
+in {
+  options = {
+    services.yaskkserv2 = {
+      enable = mkEnableOption "Enable yaskkserv2";
+      package = mkOption {
+        type = types.package;
+        default = yaskkserv2;
+      };
+      dictionary = mkOption { type = types.str; };
+      port = mkOption {
+        type = types.int;
+        default = 1178;
+      };
+      maxConnections = mkOption {
+        type = types.int;
+        default = 16;
+      };
+      listenAddress = mkOption {
+        type = types.str;
+        default = "0.0.0.0";
+      };
+      hostnameAndIpAddressForProtocol3 = mkOption {
+        type = types.str;
+        default = "localhost:127.0.0.1";
+      };
+      googleTimeoutMilliseconds = mkOption {
+        type = types.int;
+        default = 1000;
+      };
+      googleCacheFilename = mkOption {
+        type = with types; nullOr str;
+        default = null;
+      };
+      googleCacheEntries = mkOption {
+        type = types.int;
+        default = 1024;
+      };
+      googleCacheExpireSeconds = mkOption {
+        type = types.int;
+        default = 2592000;
+      };
+      googleMaxCandidatesLength = mkOption {
+        type = types.int;
+        default = 25;
+      };
+      maxServerCompletions = mkOption {
+        type = types.int;
+        default = 64;
+      };
+      googleJapaneseInput = mkOption {
+        type = types.enum [ "notfound" "disable" "last" "first" ];
+        default = "notfound";
+      };
+      googleSuggest = mkOption {
+        type = types.bool;
+        default = false;
+      };
+      googleUseHttp = mkOption {
+        type = types.bool;
+        # default: https
+        default = false;
+      };
+      midashiUtf8 = mkOption {
+        type = types.bool;
+        # default: euc
+        default = false;
+      };
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    launchd.user.agents.yaskkserv2 = {
+      script = concatStringsSep " " [
+        "${lib.getBin cfg.package}/bin/yaskkserv4 "
+        cfg.dictionary
+        "--no-daemonize"
+        "--port ${toString cfg.port}"
+        "--max-connections ${toString cfg.maxConnections}"
+        "--listen-address ${cfg.listenAddress}"
+        "--hostname-and-ip-address-for-protocol-3 ${cfg.hostnameAndIpAddressForProtocol3}"
+        "--google-timeout-milliseconds ${
+          toString cfg.googleTimeoutMilliseconds
+        }"
+        (if cfg.googleCacheFilename != null then
+          "--google-cache-filename ${cfg.googleCacheFilename}"
+        else
+          "")
+        "--google-cache-entries ${toString cfg.googleCacheEntries}"
+        "--google-cache-expire-seconds ${toString cfg.googleCacheExpireSeconds}"
+        "--google-max-candidates-length ${
+          toString cfg.googleMaxCandidatesLength
+        }"
+        "--max-server-completions ${toString cfg.maxServerCompletions}"
+        "--google-japanese-input ${cfg.googleJapaneseInput}"
+        (if cfg.googleSuggest then "--google-suggest" else "")
+        (if cfg.googleUseHttp then "--google-use-http" else "")
+        (if cfg.midashiUtf8 then "--midashi-utf8" else "")
+      ];
+      serviceConfig = {
+        KeepAlive = true;
+        RunAtLoad = true;
+      };
+    };
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  description = "yaskkserv2 service for Nix";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    yaskkserv2-src = {
+      url = "github:wachikun/yaskkserv2";
+      flake = false;
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } (_: {
+      systems = [ "aarch64-linux" "aarch64-darwin" ];
+      perSystem = { system, pkgs, lib, ... }:
+        let
+          inherit (pkgs.lib) optionals;
+          inherit (pkgs.stdenv) isDarwin;
+          toolchain = pkgs.fenix.stable.defaultToolchain;
+          craneLib = inputs.crane.lib.${system}.overrideToolchain toolchain;
+          yaskkserv2 = with craneLib; rec {
+            commonArgs = {
+              src = cleanCargoSource (path "${inputs.yaskkserv2-src}");
+              # skip test
+              checkPhaseCargoCommand = "";
+              buildInputs = with pkgs;
+                [ pkg-config libiconv ] ++ (optionals isDarwin
+                  (with pkgs.darwin.apple_sdk; [
+                    frameworks.Security
+                    frameworks.CoreFoundation
+                    frameworks.CoreServices
+                    frameworks.SystemConfiguration
+                  ]));
+            };
+            artifacts = buildDepsOnly (commonArgs // { pname = "yaskkserv2"; });
+            package =
+              buildPackage (commonArgs // { cargoArtifacts = artifacts; });
+          };
+        in {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = with inputs; [ inputs.fenix.overlays.default ];
+          };
+          packages.yaskkserv2 = yaskkserv2.package;
+          apps = {
+            yaskkserv2 = {
+              type = "app";
+              program = "${yaskkserv2.package}/bin/yaskkserv2";
+            };
+            makeDirectory = {
+              type = "app";
+              program = "${yaskkserv2.package}/bin/yaskkserv2_make_dictionary";
+            };
+          };
+        };
+    }) // {
+      darwinModules.default = import ./darwin-modules.nix self;
+    };
+}
